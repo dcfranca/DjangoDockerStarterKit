823dbbe148dbb304740874c721008458f3e64ee1
Daniel Franca <daniel.franca@protonmail.com>
2020-02-07 14:28:48 +0100

    Add custom user model with email instead of username

--- app/website/settings/base.py
+++ app/website/settings/base.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-AUTH_USER_MODEL = 'auth.User'
+AUTH_USER_MODEL = 'authentication.MyUser'
 
 # Application definition
 
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'auth',
+    'authentication',
 ]
 
 MIDDLEWARE = [
