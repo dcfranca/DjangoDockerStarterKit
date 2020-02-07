6178b17d9a2a31e55f1cf741a39c6e514296c173
Daniel Franca <daniel.franca@protonmail.com>
2020-02-07 13:56:38 +0100

    Add support to JSONApi and Custom User with email identifier

---
+++ app/auth/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext as _
+
+
+class User(AbstractUser):
+    USERNAME_FIELD = 'email'
+    email = models.EmailField(_('email address'), unique=True)
+    REQUIRED_FIELDS = []
